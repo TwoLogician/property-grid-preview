7fe58128456bd638da5755639701689a2b620b57
The Amazing Developer-Man <Two.Logician@icloud.com>
2017-08-08 10:49:22 +0700

    first commit

---
+++ PropertyGridPreview/Models/Info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGridPreview.Models
+{
+    public class Info
+    {
+        public Oid Oid { get; set; } = new Oid();
+        public Rectangle Rectangle { get; set; } = new Rectangle();
+    }
+}
